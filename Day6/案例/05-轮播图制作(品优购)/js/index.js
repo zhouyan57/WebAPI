8e2e9c21f3513dacfa1b8f2ad94c62b5986fcc15
jane_glb <994824764@qq.com>
2020-05-15 21:12:57 +0800

    第六天

--- Day6/案例/05-轮播图制作(品优购)/js/index.js
+++ Day6/案例/05-轮播图制作(品优购)/js/index.js
@@ -8,10 +8,17 @@ window.addEventListener('load', function () {
     focus.addEventListener('mouseenter', function () {
         arrow_l.style.display = 'block';
         arrow_r.style.display = 'block';
+        clearInterval(timer);
+        timer = null; // 清楚定时器变量
     })
     focus.addEventListener('mouseleave', function () {
         arrow_l.style.display = 'none';
         arrow_r.style.display = 'none';
+        timer = setInterval(function () {
+            // 手动调用点击事件
+            arrow_r.click();
+        }, 2000);
+
     })
     // 3. 动态生成小圆圈 有几张图片 就生成几个小圆圈
     var ul = focus.querySelector('ul');
@@ -36,6 +43,11 @@ window.addEventListener('load', function () {
             // ul 的移动距离 就是 小圆圈的索引号 乘以 图片的宽度 注意是负值
             // 当我们点击了某个小li 就拿到当前小li 的索引号
             var index = this.getAttribute('index');
+            // 当我们点击了某个小li 就要把这个li 的索引号给 num
+            num = index;
+            // 当我们点击了某个小li 就要把这个li 的索引号给 num
+            circle = index;
+            // num = circle = index;
             // var focusWidth = focus.offsetWidth;
             console.log(focusWidth);
             console.log(index);
@@ -45,17 +57,79 @@ window.addEventListener('load', function () {
     }
     // 把ol里面的第一个小li设置类名为 current
     ol.children[0].className = 'current';
-    // 6. 点击右侧按钮, 图片滚动一张
+    // 6. 克隆第一张图片(li)放到ul 最后面
+    var first = ul.children[0].cloneNode(true);
+    ul.appendChild(first);
+    // 7. 点击右侧按钮, 图片滚动一张
     var num = 0;
+    // circle 控制小圆圈的播放
+    var circle = 0;
+    // flag 节流阀
+    var flag = true;
     arrow_r.addEventListener('click', function () {
-        // 如果走到了最后复制的一张图片, 此时 我们的ul 要快速复原 left 改为 0
-        if (num == ul.children.length - 1) {
-            ul.style.left = 0;
-            num = 0;
+        if (flag) {
+            flag = false; // 关闭节流阀
+            // 如果走到了最后复制的一张图片, 此时 我们的ul 要快速复原 left 改为 0
+            if (num == ul.children.length - 1) {
+                ul.style.left = 0;
+                num = 0;
+            }
+            num++;
+            animate(ul, -num * focusWidth, function () {
+                flag = true;
+            });
+            // 8. 点击右侧按钮, 小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
+            circle++;
+            // 如果circle == 4 说明走到最后我们克隆的这张图片了 我们就复原
+            if (circle == ol.children.length) {
+                circle = 0;
+            }
+            // 调用函数
+            circleChange();
         }
-        num++;
-        animate(ul, -num * focusWidth);
-    })
+    });
+
+    // 9. 左侧按钮做法
+
+    arrow_l.addEventListener('click', function () {
+        if (flag) {
+            flag = false;
+            // 如果走到了最后复制的一张图片, 此时 我们的ul 要快速复原 left 改为 0
+            if (num == 0) {
+                num = ul.children.length - 1;
+                ul.style.left = -num * focusWidth + 'px';
+            }
+            num--;
+            animate(ul, -num * focusWidth, function () {
+                flag = true;
+            });
+            // 8. 点击右侧按钮, 小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
+            circle--;
+            // 如果circle < 0  说明第一张图片. 则小圆圈要改为第4个小圆圈(3)
+            // if (circle < 0) {
+            //     circle = ol.children.length - 1;
+            // }
+            circle = circle < 0 ? ol.children.length - 1 : circle;
+            // 调用函数
+            circleChange();
+        }
+
+    });
+
+    function circleChange() {
+        // 先清楚其余小圆圈的current类名
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+        // 留下当前的小圆圈的current类名
+        ol.children[circle].className = 'current';
+    }
+
+    // 10. 自动播放轮播图
+    var timer = setInterval(function () {
+        // 手动调用点击事件
+        arrow_r.click();
+    }, 2000);
 
 
 })
